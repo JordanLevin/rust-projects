aeb707a679417e14820d2594f96d642bf501cc94
Jordan Levin <jlevin38@binghamton.edu>
2017-07-18 15:03:06 -0400

    a bit less broken but still broken

--- linkedlist/src/main.rs
+++ linkedlist/src/main.rs
@@ -1,51 +1,36 @@
-enum Node {
+enum List {
     Cons(i32, Box<List>),
     Nil,
 }
 
-use Node::{Cons, Nil};
+use List::{Cons, Nil};
 
-struct List {
-    pub head: Node,
-}
-
-impl List -> List{
-    fn new(){
-        List{Node: Nil} 
+impl List {
+    fn new() -> List{
+        Nil 
     }
 
-    fn add(mut &self, val: i32){
-        let &mut current: Node = &mut self.head;
-        let mut next: Node = Nil;
-        match current {
-            Nil => {
-                current
-            },
-            Cons(num, next) => {
-                next = current.next;
-            },
-        }
-        loop {
-            match current{
-                Cons(num, next) =>{
-
-                },
-                Nil => {
-                    
-                },
-            }
-        }
+    fn prepend(self, val: i32) -> List{
+        Cons(val, Box::new(self))
     }
 
-    fn delete(&self, val: i32){
+    //fn delete(&self, val: i32){
 
-    }
+    //}
 
     fn print(&self){
+        match self{
+            &Cons(val, next) => {
+                print!("{} ", val);
+                (*next).print();
+            },
+            &Nil => {
 
+            }
+        }
     }
 }
 
 fn main(){
-    let list = List::Cons(1, Box::new(List::Cons(5, Box::new(List::Nil))));
+    let list = List::new();
 }
